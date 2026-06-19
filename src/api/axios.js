76f8e9a8fc20bc8cfd6c0a93b04650887d660b87
Gautam <gautam788gaud@gmail.com>
2026-06-19 17:27:58 +0530

    third chatapp frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://chatapp-backend-seven-tan.vercel.app/api",
+  baseURL: "https://chatapp-backend-3exy.onrender.com/api",
 });
 
 api.interceptors.request.use(
